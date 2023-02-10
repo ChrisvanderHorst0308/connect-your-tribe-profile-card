2ac1218e4d6a6a9db694942510edffdd21d41474
ChrisvanderHorst0308 <112861116+ChrisvanderHorst0308@users.noreply.github.com>
2023-02-10 09:59:54 +0100

    -

---
+++ public/scripts/profile-card.js
@@ -0,0 +1,15 @@
+// window.addEventListener('scroll' , function(){
+//     const BgImg = document.querySelector('header img');
+//     let WinPos  = window.pageYOffset;
+
+//     BgImg.style.transform = "translateY(" + WinPos * .35 + "px)";
+// });
+
+
+// window.addEventListener('scroll', function(){
+//     const BgImg = document.querySelector('header > img');
+//     let WinPos = window.pageYOffset;
+
+
+//     BgImg.style.transform = "translateY(" + WinPos * .35 + 'px')";
+// })
